fb1bff978af07b344c7748545e623a45c6ce760d
jurnalp <journalpraktikum@gmail.com>
2020-09-14 20:38:26 +0700

    add paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
 	<h3>Apa kabar</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui officiis quam quia sapiente quisquam omnis labore repudiandae in delectus voluptate saepe, nobis molestiae sit quidem nam nostrum ipsum deserunt eius! </p>
 </body>
 </html>
